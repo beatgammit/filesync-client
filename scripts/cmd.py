9dc0119fca16a8c7d1d81cf89d0efe2f4554d7d8
Jameson Little <t.jameson.little@gmail.com>
2011-08-08 17:46:34 -0600

    Simplified python scripts some

--- scripts/cmd.py
+++
@@ -1,8 +0,0 @@
-# Run a command and get stdout, stderr and returncode
-class Command(object):
-	def __init__(self, command, shell=True):
-		import subprocess as sp
-
-		process = sp.Popen(command, shell = shell, stdout = sp.PIPE, stderr = sp.PIPE)
-		self.stdout, self.stderr = process.communicate()
-		self.returncode = process.returncode
